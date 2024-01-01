40754013d7c7640e2b3996f62d6cf483968a191f
fathyElgazar <gazzarDev@gmail.com>
2024-01-01 06:13:54 +0200

    Pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...elements) {
+  return arr.filter((i) => !elements.includes(i));
 };
 
 // Do not edit below this line
