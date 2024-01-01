8ea1a1c93e155c8a4d95b1f57339a05b17a13edc
fathyElgazar <gazzarDev@gmail.com>
2024-01-01 04:31:53 +0200

    Pass test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let splitInput = input.split("");
+  reverseInput = splitInput.reverse();
+  return reverseInput.join("");
 };
 
 // Do not edit below this line
