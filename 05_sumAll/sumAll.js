71c0f4bda6fa026d0d22c1bd6d4544a16338075a
fathyElgazar <gazzarDev@gmail.com>
2024-01-01 06:49:26 +0200

    Pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (typeof int1 != "number" || typeof int2 != "number") return "ERROR";
 
+  if (int1 < 0 || int2 < 0) return "ERROR";
+
+  let sum = 0;
+
+  if (int1 > int2) {
+    for (let i = int2; i <= int1; i++) {
+      sum += i;
+    }
+  }
+
+  for (let i = int1; i <= int2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
