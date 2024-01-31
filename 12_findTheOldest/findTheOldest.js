d6fd3daf5ce31647baa8feff9d31c0b26a75df65
fathyElgazar <gazzarDev@gmail.com>
2024-01-31 11:55:29 +0200

    feat: Try hard on the test solve some, cheat some

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((oldest, cPerson) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let currentAge = getAge(cPerson.yearOfBirth, cPerson.yearOfDeath);
 
+    return oldestAge > currentAge ? oldest : cPerson;
+  });
+};
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
