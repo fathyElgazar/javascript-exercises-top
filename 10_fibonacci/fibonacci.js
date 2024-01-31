1dcfd39168738dfa7bd8e09feb2d6018cd8e8058
fathyElgazar <gazzarDev@gmail.com>
2024-01-31 09:43:21 +0200

    feat: cheat for the solution bc so hard

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  if (number == 0) return 0;
+  if (typeof number === "string") Number(number);
+  let count = number;
+  let firstPrev = 1;
+  let secondPrev = 0;
 
+  for (let i = 2; i <= count; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
